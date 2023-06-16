7a2e1ef8a0079aa73daa2a055199c36945dd7c3c
Crowdbotics <team@crowdbotics.com>
2023-06-16 22:16:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "healthy-hair-app-41550",
+  url: "https://healthy-hair-app-41550.botics.co",
+  api: "https://healthy-hair-app-41550.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
